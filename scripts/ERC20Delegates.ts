3c684e728bdf8fba9fbe471204ab8f3bbeef32da
78carla <78c.carla@gmail.com>
2023-03-06 12:01:29 +0100

    Delegate token update

--- scripts/ERC20Delegates.ts
+++ scripts/ERC20Delegates.ts
@@ -31,26 +31,27 @@ async function main() {
   const contract = await contractFactory.attach(tokenAddress);
   console.log("Successfully attached");
 
-  //Check the voting power
-  const votePower = await contract.getVotes(signer.address);
-  console.log("Voting power is", ethers.utils.formatEther(votePower));
-
-  const tokenBalanceAccount = await contract.balanceOf(signer.address);
-  console.log(
-    "The signer has a balance of",
-    ethers.utils.formatEther(tokenBalanceAccount),
-    "vote tokens!"
-  );
-
   if (args.length === 3) {
     //Set the self-delegate
-    const delegateTx = await contract.delegate(signer.address);
-    const delegateTxReceipt = await delegateTx.wait();
+
+    //Check the voting power
+    const votePowerAccount = await contract.getVotes(signer.address);
+    console.log("Voting power is", ethers.utils.formatEther(votePowerAccount));
+
+    const tokenBalanceAccount = await contract.balanceOf(signer.address);
+    console.log(
+      "The signer has a balance of",
+      ethers.utils.formatEther(tokenBalanceAccount),
+      "vote tokens!"
+    );
+
+    const delegateTxAccount = await contract.delegate(signer.address);
+    const delegateTxReceiptAccount = await delegateTxAccount.wait();
     console.log(
       "Token delegate to",
       signer.address,
       "at block number",
-      delegateTxReceipt.blockNumber
+      delegateTxReceiptAccount.blockNumber
     );
 
     //Check the voting power of the account
@@ -65,44 +66,49 @@ async function main() {
   } else {
     //Delegate to delegateAddress
     const delegateAddress = args[3];
-    const delegateValue = ethers.utils.parseEther(args[4]);
-    const transferTx = await contract
-      .connect(delegateAddress)
-      .transfer(delegateAddress, delegateValue);
 
-    console.log(delegateAddress);
-    console.log(delegateValue);
-    console.log(transferTx);
-
-    const transferTxReceipt = await transferTx.wait();
-    console.log(
-      ethers.utils.formatEther(delegateValue),
-      "Token transfered from",
-      signer.address,
-      "to",
-      delegateAddress,
-      "at block number",
-      transferTxReceipt.blockNumber
+    const tokenBalanceDelegateBefore = await contract.balanceOf(
+      delegateAddress
     );
-
-    const tokenBalanceDelegate = await contract
-      .connect(delegateAddress)
-      .balanceOf(delegateAddress);
     console.log(
       "Delegate",
       delegateAddress,
       "has a balance of",
-      ethers.utils.formatEther(tokenBalanceDelegate),
+      ethers.utils.formatEther(tokenBalanceDelegateBefore),
+      "vote tokens!"
+    );
+
+    const votePowerDelegateBefore = await contract.getVotes(delegateAddress);
+    console.log(
+      "Delegate voting power is",
+      ethers.utils.formatEther(votePowerDelegateBefore),
+      "\n"
+    );
+
+    const delegateTxDelegate = await contract.delegate(delegateAddress);
+    const delegateTxReceiptDelegate = await delegateTxDelegate.wait();
+
+    console.log(
+      "Token delegate to",
+      delegateAddress,
+      "at block number",
+      delegateTxReceiptDelegate.blockNumber
+    );
+
+    const tokenBalanceDelegateAfter = await contract.balanceOf(delegateAddress);
+    console.log(
+      "Delegate",
+      delegateAddress,
+      "has a balance of",
+      ethers.utils.formatEther(tokenBalanceDelegateAfter),
       "vote tokens!"
     );
 
     //Check the voting power of the delegate
-    const votePowerDelegate = await contract
-      .connect(delegateAddress)
-      .getVotes(delegateAddress);
+    const votePowerDelegateAfter = await contract.getVotes(delegateAddress);
     console.log(
       "Delegate voting power is",
-      ethers.utils.formatEther(delegateAddress),
+      ethers.utils.formatEther(votePowerDelegateAfter),
       "\n"
     );
   }
